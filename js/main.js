45e01263137588611c2207aae5f1bedf2ba96795
dltnk9 <dltnk9@naver.com>
2022-02-26 01:44:54 +1100

    youtube video inserted

--- js/main.js
+++ js/main.js
@@ -52,3 +52,37 @@ new Swiper('.notice-line .swiper-container', {
   autoplay: true,
   loop: true
 });
+
+new Swiper('.promotion .swiper-container', {
+  // 기본값 horizontal
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay : 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function(){
+isHidePromotion = !isHidePromotion
+//계속 반대값을 반환
+if(isHidePromotion) {
+  promotionEl.classList.add('hide');
+}else {
+  promotionEl.classList.remove('hide');
+
+}
+
+});
