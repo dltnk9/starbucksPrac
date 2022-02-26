0c0215a22364944c9516372e61c0f5bd14dd5903
dltnk9 <dltnk9@naver.com>
2022-02-27 06:59:36 +1100

    cant find the solution still working on it

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ searchInputEl.addEventListener('blur', function(){
 
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function (){
   console.log(window.scrollY);
@@ -26,16 +27,28 @@ window.addEventListener('scroll', _.throttle(function (){
       opacity: 0,
       display: 'none'
     });
+    gsap.to(toTopEl, 0.2, {
+      x: 0
+    });
   }else {
     badgeEl.style.display = 'block';
     gsap.to(badgeEl, 0.6, {
       opacity: 1,
       display: 'block'
   });
+  gsap.to(toTopEl, 0.2, {
+    x: 100
+  });
 }
 
 }, 300));
 
+
+toTopEl.addEventListener('click', function() {
+gsap.to(window, .7, {
+  scrollTo: 0
+});
+});
 // _.throttle(함수, 시간)
 
 
@@ -71,6 +84,18 @@ new Swiper('.promotion .swiper-container', {
     nextEl: '.promotion .swiper-next'
   }
 });
+
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop :true,
+  spaceBetween:30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awrads .swiper-prev',
+    nextEl: '.awrads .swiper-next'
+  }
+});
+
 
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
@@ -118,3 +143,6 @@ spyEls.forEach(function (spyEl){
   .setClassToggle(spyEl, 'show')
   .addTo(new ScrollMagic.Controller());
 })
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); 
