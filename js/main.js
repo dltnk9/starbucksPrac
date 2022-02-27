a7f03349db9537541094273d0d1445c87bf82a2c
dltnk9 <dltnk9@naver.com>
2022-02-27 17:08:19 +1100

    공통모듈분리

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-const searchEl = document.querySelector('.search')
-const searchInputEl = searchEl.querySelector('input')
-// 검색창 요소를 클릭하면 실행.
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus()
-})
-
-searchInputEl.addEventListener('focus', function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 
 const badgeEl = document.querySelector('header .badges');
@@ -144,5 +128,3 @@ spyEls.forEach(function (spyEl){
   .addTo(new ScrollMagic.Controller());
 })
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); 
